431147e61ab50e85350c218006e980a0001ed176
Efraim Nabil <efraimefefraim@gmail.com>
2024-06-17 02:44:26 +0300

    Center SVG in the Button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -7,7 +7,7 @@ interface IProps extends HTMLAttributes<HTMLButtonElement> {
 
 const Button = ({name, children, ...props}: IProps) => {
   return (
-    <button className="flex gap-2 justify-center text-[#E1E1E1] bg-gradient-to-r from-[#101C49] to-[#000000] py-3 px-4 rounded-3xl" {...props}>
+    <button className="flex gap-2 items-center text-[#E1E1E1] bg-gradient-to-r from-[#101C49] to-[#000000] py-3 px-4 rounded-3xl" {...props}>
         {children} {name}
     </button>
   )
