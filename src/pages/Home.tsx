4225eacc3c772d523c61633f884c3291ecd4bbe5
alaakorda <alaazakaria436@gmail.com>
2024-06-20 04:56:03 +0300

    chore: Update home page button styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,17 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-[#101C49] to-[#000000] " >
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-[#101C49] to-[#000000] ">
       Home
       <button
-        className="border #FFFFFF p-2 mt-2" style={{ border: '0.5 px solid #E1E1E1', borderRadius: '30px', color: '#E1E1E1',height:'59px',width: '235px'}}
+        className="border #FFFFFF p-2 mt-2"
+        style={{
+          border: "0.5 px solid #E1E1E1",
+          borderRadius: "30px",
+          color: "#E1E1E1",
+          height: "59px",
+          width: "235px",
+        }}
         onClick={() => {
           navigate("/dashboard");
         }}
