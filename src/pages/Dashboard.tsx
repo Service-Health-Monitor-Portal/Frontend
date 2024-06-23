edc7f02a206d767453adafc771fbbeb337a4e78b
Efraim Nabil <efraimefefraim@gmail.com>
2024-06-23 23:55:43 +0300

    Refactor Dashboard component to handle missing id parameter

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,12 +3,12 @@ import ChartBox from "../components/Dashboard/ChartBox"
 
 const Dashboard = () => {
   const id = useParams<{id: string}>().id
-  if(!id) return <h1>Please choose service to open it</h1>
   return (
     <div className="flex flex-col w-full items-center justify-center min-h-screen bg-gradient-to-br from-[#3A5491] to-[#182655] text-white">
-      {id}
-      Dashboard
-      <ChartBox />
+
+      {
+        !id ? <h1>Please choose service to open it</h1> : <ChartBox />
+      }
     </div>
   )
 }
