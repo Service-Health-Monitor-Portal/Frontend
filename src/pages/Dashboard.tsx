f46893e57ef76c1103f82de3ad01a44411214655
Efraim Nabil <efraimefefraim@gmail.com>
2024-06-23 23:53:51 +0300

    Update Tailwind CSS configuration and add ApexCharts dependencies

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom"
+import ChartBox from "../components/Dashboard/ChartBox"
 
 const Dashboard = () => {
   const id = useParams<{id: string}>().id
   if(!id) return <h1>Please choose service to open it</h1>
   return (
-    <div>
+    <div className="flex flex-col w-full items-center justify-center min-h-screen bg-gradient-to-br from-[#3A5491] to-[#182655] text-white">
       {id}
       Dashboard
+      <ChartBox />
     </div>
   )
 }
