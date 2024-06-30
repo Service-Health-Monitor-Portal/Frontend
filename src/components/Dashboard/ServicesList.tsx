c304b93d3379b02f7e553c01ad228f059556ad4c
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-06-30 08:41:32 +0300

    Handle GET request for getting the list of services

--- src/components/Dashboard/ServicesList.tsx
+++ src/components/Dashboard/ServicesList.tsx
@@ -1,17 +1,41 @@
-import { NavLink } from "react-router-dom";
-import services from "../../data/services"
+import { NavLink } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+interface IServiceMetadata {
+  name: string
+  id: number
+}
 
 const ServicesList = () => {
-  console.log(services);
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/influxdb/services')
+      .then((res) => {
+        console.log(res.data)
+        setServices(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
   return (
     <div className="flex flex-col text-center gap-3 text-[#E1E1E1] font-semibold w-full">
-        {services.map((service, index) => {
-            return (
-                <NavLink to={service.id} key={index} className={({isActive}) => `${isActive? 'bg-gradient-to-r from-[#101C49] to-[#000000]' : 'bg-transparent'} w-full py-3` }>
-                    {service.name}
-                </NavLink>
-            )
-        })}
+      {services.map((service: IServiceMetadata, index) => {
+        return (
+          <NavLink
+            to={service.id.toString()}
+            key={index}
+            className={({ isActive }) =>
+              `${isActive ? 'bg-gradient-to-r from-[#101C49] to-[#000000]' : 'bg-transparent'} w-full py-3`
+            }
+          >
+            {service.name}
+          </NavLink>
+        )
+      })}
     </div>
   )
 }
