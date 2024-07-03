7b25fd7af2a73a0a8befe820498f08aba3a1dbcf
Efraim Nabil <efraimefefraim@gmail.com>
2024-07-04 02:09:56 +0300

    Refactor ServicesList component to use React Query and add query client provider.

--- src/components/Dashboard/ServicesList.tsx
+++ src/components/Dashboard/ServicesList.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import useCustomQuery from '../../hooks/useCustomQuery'
 
 interface IServiceMetadata {
   name: string
@@ -10,32 +10,36 @@ interface IServiceMetadata {
 const ServicesList = () => {
   const [services, setServices] = useState([])
 
+  const { data, isLoading } = useCustomQuery({
+    queryKey: ['services'],
+    url: 'services',
+  })
+
   useEffect(() => {
-    axios
-      .get('http://localhost:8080/influxdb/services')
-      .then((res) => {
-        console.log(res.data)
-        setServices(res.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+    if (data) {
+      setServices(data)
+    }
+  }, [data])
+
   return (
     <div className="flex flex-col text-center gap-3 text-[#E1E1E1] font-semibold w-full">
-      {services.map((service: IServiceMetadata, index) => {
-        return (
-          <NavLink
-            to={service.id.toString()}
-            key={index}
-            className={({ isActive }) =>
-              `${isActive ? 'bg-gradient-to-r from-[#101C49] to-[#000000]' : 'bg-transparent'} w-full py-3`
-            }
-          >
-            {service.name}
-          </NavLink>
-        )
-      })}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        services.map((service: IServiceMetadata, index) => {
+          return (
+            <NavLink
+              to={service.id.toString()}
+              key={index}
+              className={({ isActive }) =>
+                `${isActive ? 'bg-gradient-to-r from-[#101C49] to-[#000000]' : 'bg-transparent'} w-full py-3`
+              }
+            >
+              {service.name}
+            </NavLink>
+          )
+        })
+      )}
     </div>
   )
 }
