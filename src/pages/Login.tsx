b15095ec65c2c929c5226ac16f935f45843df274
alaakorda <alaazakaria436@gmail.com>
2024-08-09 03:23:58 +0300

    Added Login page: Implemented the Login component with a responsive layout using Tailwind CSS.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,16 +6,16 @@ const Login = () => {
   useParams<{ id: string }>()
   const navigate = useNavigate()
   return (
-    <div className="flex flex-col lg:flex-row justify-between h-full py-5 px-10 bg-gradient-to-r from-[#2C427F] to-[#101C49]">
+    <div className="flex flex-col lg:flex-row justify-between h-full py-10 px-10 bg-gradient-to-r from-[#2C427F] to-[#101C49]">
       <div className="flex flex-col gap-5">
-        <label className="text-xl md:text-2xl text-white">Email</label>
+        <label className="text-xl md:text-2xl text-[#E1E1E1]">Email</label>
         <input
           className="text-lg md:text-lg text-white bg-transparent border border-[#E1E1E1] rounded-[10px] h-[38px] w-[267px] p-4 focus:outline-none focus:border-[#E1E1E1]"
           type="email"
           placeholder="Enter Email"
         />
 
-        <label className="text-xl md:text-2xl text-white">Password</label>
+        <label className="text-xl md:text-2xl text-[#E1E1E1]">Password</label>
         <input
           className="text-lg md:text-lg text-white bg-transparent border border-[#E1E1E1] rounded-[10px] h-[38px] w-[267px] p-4 focus:outline-none focus:border-[#E1E1E1]"
           type="password"
@@ -32,7 +32,7 @@ const Login = () => {
           <h1 className="text-lg">Login</h1>
         </button>
       </div>
-      <img src={loginImage} alt="login" className="w-full lg:w-3/6 pt-28 px-12" />
+      <img src={loginImage} alt="login" className="flex justify-end w-full lg:w-4/6 pt-10 pl-20" />
     </div>
   )
 }
