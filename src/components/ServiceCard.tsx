f4b549cd3b763d28ac5eb03ed00b8854522aeafe
efraimnabil <efraimefefraim@gmail.com>
2024-09-23 19:53:13 +0300

    Add Dropdown Menu for Service Card on click at Ellipsis

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,25 +1,33 @@
 import { Ellipsis } from "lucide-react";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 interface IProps {
     title: string;
     description?: string;
-    badges?: string[]
+    badges?: string[];
 }
 
 export default function ServiceCard({ title, description, badges }: IProps) {
     return (
-        <Card className="w-full">
+        <Card className="relative w-full">
             <CardHeader className="relative">
                 <div className="space-y-1">
                     <CardTitle>{title}</CardTitle>
                     <CardDescription>{description}</CardDescription>
                 </div>
-                
-                <Ellipsis className="absolute top-3 right-5 h-5 w-5 cursor-pointer" />
+                <DropdownMenu>
+                    <DropdownMenuTrigger className="absolute top-3 right-5">
+                        <Ellipsis className="h-5 w-5 cursor-pointer" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="absolute right-0 z-10">
+                        <DropdownMenuItem>Update Service</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Delete Service</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </CardHeader>
-            
             <CardContent className="flex flex-wrap gap-2">
                 {badges?.map((badge, index) => (
                     <Badge key={index}>{badge}</Badge>
